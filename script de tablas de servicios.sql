229dfa97af24f193445c22d1d964ac92fc857f77
devJCM <jesus.carrillo@tactos.com.mx>
2019-02-27 17:34:16 -0600

    version_3.0|Se actualizan scripts de Mysql para la creacion de la tabla nbo_in

--- script de tablas de servicios.sql
+++ script de tablas de servicios.sql
@@ -1,4 +1,5 @@
 drop table if exists rfm_in,rfm_out,clv_out;
+drop table if exists nbo_in;
 
 -- rfm_in --
 CREATE TABLE IF NOT EXISTS rfm_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -7,7 +8,7 @@ CREATE TABLE IF NOT EXISTS rfm_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KE
                                    Nombre VARCHAR(125),
                                    Fecha datetime,
                                    Monto double(28,6));
---     
+--   
 -- rfm_out --
 CREATE TABLE IF NOT EXISTS rfm_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 								   Ejecucion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -32,9 +33,28 @@ CREATE TABLE IF NOT EXISTS clv_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY K
                                    Vida_cliente double(16,4),
                                    CLV double(28,6));
 --
+-- nbo_in --
+CREATE TABLE IF NOT EXISTS nbo_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                   Id_cliente VARCHAR(125),
+                                   Nombre VARCHAR(125),
+                                   Macro_sector CHAR(5),
+                                   Sector char(12),
+                                   Subsector char(20),
+                                   Actividad char(12),
+                                   Ventas double(16,4),
+                                   Empleados char(20),
+                                   Activo_fijo double(16,4),
+                                   Potencial double(16,4),
+                                   Cheques double(16,4),
+                                   Etapa CHAR(5),
+                                   Producto char(20),
+                                   Vigencia date
+                                   );
+-- 
 select * from rfm_in;
 select * from rfm_out;
 select * from clv_out;
+select * from nbo_in;
 
 -- select fix
 SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
@@ -50,3 +70,10 @@ SELECT Id_cliente,Nombre,max(Fecha) as 'Recencia',count(Monto) as 'Frecuencia',A
 insert into rfm_in(Id_cliente,Nombre,Fecha,Monto)
 SELECT a.id,a.name,op.date_entered,op.amount FROM accounts a, opportunities op,accounts_opportunities ao WHERE a.id=ao.account_id AND op.id=ao.opportunity_id;                                   
 --
+-- llenar datos de nbo_in
+SET sql_mode = '';
+insert into nbo_in(Id_cliente,Macro_sector,Sector,Subsector,Actividad,Ventas,Empleados,Activo_fijo,Potencial,Cheques,Etapa,Producto,Vigencia)
+select a.id_c,a.tct_macro_sector_ddw_c,a.sectoreconomico_c,a.subsectoreconomico_c,a.actividadeconomica_c,a.ventas_anuales_c,
+		a.empleados_c,a.activo_fijo_c,a.potencial_cuenta_c,a.tct_prom_cheques_cur_c,
+        op.tct_etapa_ddw_c,op.tipo_producto_c,op.vigencialinea_c
+FROM accounts_cstm a, opportunities_cstm op,accounts_opportunities ao WHERE a.id_c=ao.account_id AND op.id_c=ao.opportunity_id;
