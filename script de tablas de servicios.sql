ec6c8f245733120b7d9e72e947cda61379bcf051
devJCM <jesus.carrillo@tactos.com.mx>
2019-03-11 13:12:45 -0600

    NBO|Ajuste de scripts de Mysql

--- script de tablas de servicios.sql
+++ script de tablas de servicios.sql
@@ -7,7 +7,6 @@ drop table if exists acreedor_out;
 CREATE TABLE IF NOT EXISTS rfm_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 								   Ejecucion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                    Id_cliente VARCHAR(125),
-                                   Nombre VARCHAR(125),
                                    Fecha datetime,
                                    Monto double(28,6));
 --   
@@ -37,7 +36,6 @@ CREATE TABLE IF NOT EXISTS clv_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY K
 SET sql_mode = '';
 CREATE TABLE IF NOT EXISTS nbo_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                    Id_cliente VARCHAR(125),
-                                   Nombre VARCHAR(125),
                                    Macro_sector CHAR(5),
                                    Sector char(12),
                                    Subsector char(20),
@@ -48,16 +46,14 @@ CREATE TABLE IF NOT EXISTS nbo_in (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KE
                                    Potencial double(16,4),
                                    Cheques double(16,4),
                                    Etapa CHAR(5),
-                                   Producto char(20),
-                                   Vigencia date
+                                   Producto char(20)
                                    );
 
 CREATE TABLE IF NOT EXISTS nbo_model select * from nbo_in;
 
-alter table nbo_in drop column Etapa,drop column Producto,drop column Vigencia;
+alter table nbo_in drop column Etapa,drop column Producto;
 --
 -- nbo_out --
-SET sql_mode = '';
 CREATE TABLE IF NOT EXISTS nbo_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 								   Ejecucion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                    Id_cliente VARCHAR(125),
@@ -70,7 +66,6 @@ CREATE TABLE IF NOT EXISTS nbo_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY K
                                    );
 ---
 -- acreedor_out --
-SET sql_mode = '';
 CREATE TABLE IF NOT EXISTS acreedor_out (Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                    Ejecucion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                    Id_cliente VARCHAR(125),
@@ -94,15 +89,15 @@ select distinct Id_cliente from rfm_in;
 SELECT TIMESTAMPDIFF(MONTH,(select MAX(Fecha) FROM rfm_in WHERE Id_cliente='e45d4e71-22c6-421d-85ec-77ecaead04e9') , (select MIN(Fecha) FROM rfm_in WHERE Id_cliente='e45d4e71-22c6-421d-85ec-77ecaead04e9'));
 --
 -- select de RFM
-SELECT Id_cliente,Nombre,max(Fecha) as 'Recencia',count(Monto) as 'Frecuencia',AVG(Monto) as 'Monto' FROM rfm_in group by Id_cliente;
+SELECT Id_cliente,max(Fecha) as 'Recencia',count(Monto) as 'Frecuencia',AVG(Monto) as 'Monto' FROM rfm_in group by Id_cliente;
 --
 -- llenar datos de rfm_in
-insert into rfm_in(Id_cliente,Nombre,Fecha,Monto)
-SELECT a.id,a.name,op.date_entered,op.amount FROM accounts a, opportunities op,accounts_opportunities ao WHERE a.id=ao.account_id AND op.id=ao.opportunity_id;                                   
+insert into rfm_in(Id_cliente,Fecha,Monto)
+SELECT a.id,op.date_entered,op.amount FROM accounts a, opportunities op,accounts_opportunities ao WHERE a.id=ao.account_id AND op.id=ao.opportunity_id;                                   
 --
--- llenar datos de nbo_in
+-- llenar datos de nbo_model
 SET sql_mode = '';
-insert into nbo_in(Id_cliente,Macro_sector,Sector,Subsector,Actividad,Ventas,Empleados,Activo_fijo,Potencial,Cheques,Etapa,Producto,Vigencia)
+insert into nbo_model(Id_cliente,Macro_sector,Sector,Subsector,Actividad,Ventas,Empleados,Activo_fijo,Potencial,Cheques,Etapa,Producto,Vigencia)
 select a.id_c,a.tct_macro_sector_ddw_c,a.sectoreconomico_c,a.subsectoreconomico_c,a.actividadeconomica_c,a.ventas_anuales_c,
 		a.empleados_c,a.activo_fijo_c,a.potencial_cuenta_c,a.tct_prom_cheques_cur_c,
         op.tct_etapa_ddw_c,op.tipo_producto_c,op.vigencialinea_c
